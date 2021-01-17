a2079676fafa47d8ede5666ea6ac497670a57b1b
andrevieiragoncalves <vgoncalves.andre@gmail.com>
2021-01-17 17:47:48 -0300

    removendo imports

--- app/src/main/java/com/picpay/desafio/android/CustomApplication.kt
+++ app/src/main/java/com/picpay/desafio/android/CustomApplication.kt
@@ -4,9 +4,6 @@ import android.app.Application
 import com.picpay.desafio.android.di.contactsViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
-import org.koin.core.Koin
-import org.koin.core.KoinApplication
-import org.koin.core.KoinComponent
 import org.koin.core.context.startKoin
 
 class CustomApplication : Application() {
--- app/src/main/java/com/picpay/desafio/android/repository/ContactRepositoryImpl.kt
+++ app/src/main/java/com/picpay/desafio/android/repository/ContactRepositoryImpl.kt
@@ -2,7 +2,6 @@ package com.picpay.desafio.android.repository
 
 import com.picpay.desafio.android.model.User
 import com.picpay.desafio.android.network.PicPayService
-import com.picpay.desafio.android.network.RetrofitService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
